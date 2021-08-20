834a7fc09a62f78ce86ded381e81f8a8d675cb25
Iman Homayouni <e2ma3n@gmail.com>
2021-08-20 13:46:40 +0430

    Update vnc.installer.sh

--- vnc.installer.sh
+++ vnc.installer.sh
@@ -4,13 +4,12 @@
 # Email : homayouni.iman@Gmail.com
 # Website : http://www.homayouni.info
 # License : GPL v2.0
-# Last update : 11-March-2021_19:53:05
-# vnc_installer v1.0.1
+# Last update : Fri, 20 Aug 2021 12:20:48 +0430
+# vnc.installer v1.0.2
 # ------------------------------------------------------------------------------------------------------- #
-# SUCCESSFULLY TESTED IN UBUNTU 16.04 [XENIAL]
-# SUCCESSFULLY TESTED IN UBUNTU 18.04 [BIONIC]
-# SUCCESSFULLY TESTED IN UBUNTU 20.04 [FOCAL]
-# SUCCESSFULLY TESTED IN DEBIAN 10.X [BUSTER]
+# SUCCESSFULLY TESTED IN UBUNTU 18.04 [BIONIC]   [XFCE and Mate Desktop]
+# SUCCESSFULLY TESTED IN UBUNTU 20.04 [FOCAL]    [Only XFCE Desktop]
+# SUCCESSFULLY TESTED IN DEBIAN 10.X  [BUSTER]   [XFCE and Mate Desktop]
 # ------------------------------------------------------------------------------------------------------- #
 
 
@@ -31,9 +30,9 @@ apt-get -y -f install
 
 
 # SELECT DESKTOP ENVIRONMENT # -------------------------------------------------------------------------- #
-echo -e "[>] --------------------------------------------------------------------------------- [<]"
-echo -e "[1] INSTALL MATE DESKTOP"
-echo -e "[2] INSTALL XFCE DESKTOP"
+echo -e  "[>] --------------------------------------------------------------------------------- [<]"
+echo -e  "[1] INSTALL MATE DESKTOP"
+echo -e  "[2] INSTALL XFCE DESKTOP"
 echo -en "[?] WHICH ONE ? [1/2] : " ; read q
 
 if [ "$q" = "1" ] ; then
@@ -69,6 +68,45 @@ apt-get install -y libxss1
 # ------------------------------------------------------------------------------------------------------- #
 
 
+# CREATE VNC DIRECTORY # -------------------------------------------------------------------------------- #
+mkdir ~/.vnc/ &> /dev/null
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# CREATE xstartup FILE # -------------------------------------------------------------------------------- #
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak &> /dev/null
+
+if [ "$q" = "1" ] ; then
+
+    echo '#!/bin/bash'                                            > ~/.vnc/xstartup
+    echo -e 'unset DBUS_SESSION_BUS_ADDRESS'                     >> ~/.vnc/xstartup
+    echo -e '[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup' >> ~/.vnc/xstartup
+    echo -e '[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources' >> ~/.vnc/xstartup
+    echo -e 'xsetroot -solid grey'                               >> ~/.vnc/xstartup
+    echo -e 'vncconfig -iconic &'                                >> ~/.vnc/xstartup
+    echo -e 'x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &' \
+    >> ~/.vnc/xstartup
+    echo -e 'x-window-manager &'                                 >> ~/.vnc/xstartup
+    echo -e 'mate-session &'                                     >> ~/.vnc/xstartup
+
+elif [ "$q" = "2" ] ; then
+
+    echo '#!/bin/bash'                                            > ~/.vnc/xstartup
+    echo 'PATH=/usr/bin:/usr/sbin'                               >> ~/.vnc/xstartup
+    echo 'unset SESSION_MANAGER'                                 >> ~/.vnc/xstartup
+    echo 'unset DBUS_SESSION_BUS_ADDRESS'                        >> ~/.vnc/xstartup
+    echo 'xrdb $HOME/.Xresources'                                >> ~/.vnc/xstartup
+    echo 'startxfce4 &'                                          >> ~/.vnc/xstartup
+
+fi
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# SET EXECUTE PERMISSIONS FOR ~/.vnc/xstartup # --------------------------------------------------------- #
+chmod +x ~/.vnc/xstartup
+# ------------------------------------------------------------------------------------------------------- #
+
+
 # RUN vncserver COMMAND # ------------------------------------------------------------------------------- #
 for (( ;; )) ; do
     vncserver
@@ -87,7 +125,7 @@ done
 
 
 # CREATE vnc.kill SCRIPT # ------------------------------------------------------------------------------ #
-echo 'for (( i=1 ; i <= 10 ; i++ )) ; do' > /root/vnc.kill
+echo 'for (( i=1 ; i <= 10 ; i++ )) ; do'    > /root/vnc.kill
 echo '    vncserver -kill :$i &> /dev/null' >> /root/vnc.kill
 echo 'done' >> /root/vnc.kill
 # ------------------------------------------------------------------------------------------------------- #
@@ -98,34 +136,7 @@ chmod +x /root/vnc.kill
 # ------------------------------------------------------------------------------------------------------- #
 
 
-# CREATE xstartup FILE # -------------------------------------------------------------------------------- #
-mv ~/.vnc/xstartup ~/.vnc/xstartup.bak &> /dev/null
-
-if [ "$q" = "1" ] ; then
-
-    echo '#!/bin/bash' > ~/.vnc/xstartup
-    echo -e 'unset DBUS_SESSION_BUS_ADDRESS' >> ~/.vnc/xstartup
-    echo -e '[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup' >> ~/.vnc/xstartup
-    echo -e '[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources' >> ~/.vnc/xstartup
-    echo -e 'xsetroot -solid grey' >> ~/.vnc/xstartup
-    echo -e 'vncconfig -iconic &' >> ~/.vnc/xstartup
-    echo -e 'x-terminal-emulator -geometry 80x24+10+10 -ls -title "$VNCDESKTOP Desktop" &' \
-    >> ~/.vnc/xstartup
-    echo -e 'x-window-manager &' >> ~/.vnc/xstartup
-    echo -e 'mate-session &' >> ~/.vnc/xstartup
-
-elif [ "$q" = "2" ] ; then
-
-    echo '#!/bin/bash' > ~/.vnc/xstartup
-    echo 'xrdb $HOME/.Xresources' >> ~/.vnc/xstartup
-    echo 'startxfce4 &' >> ~/.vnc/xstartup
-
-fi
-# ------------------------------------------------------------------------------------------------------- #
-
-
 # CREATE vnc.start FILE # ------------------------------------------------------------------------------- #
-chmod +x ~/.vnc/xstartup
 echo '/usr/bin/vncserver -localhost -depth 24 -geometry 1250x750' > /root/vnc.start
 chmod +x /root/vnc.start
 # ------------------------------------------------------------------------------------------------------- #
@@ -144,10 +155,10 @@ mv /etc/motd /etc/motd.backup &> /dev/null
 # CREATE /etc/motd FILE # ------------------------------------------------------------------------------- #
 cat << EOF > /etc/motd
 [>] --------------------------------------------------------------------------------- [<]
-[>] for start vnc server : bash /root/vnc.start
-[>] for stop vnc server : bash /root/vnc.kille
-[>] vnc start at localhost:5901 for more security
-[>] connect to server using ssh tunnel
+[>] To start vnc server : bash /root/vnc.start
+[>] To stop vnc server  : bash /root/vnc.kille
+[>] VNC server run in localhost:5901
+[>] Connect to server using ssh tunnel
 EOF
 # ------------------------------------------------------------------------------------------------------- #
 
@@ -161,7 +172,7 @@ done
 
 # CREATE /etc/motd FILE # ------------------------------------------------------------------------------- #
 cat << EOF >> /etc/motd
-[>] open remmina in your client [laptop or pc] and connect to localhost:5901
+[>] Open remmina in your client [laptop or pc] and connect to localhost:5901
 [>] --------------------------------------------------------------------------------- [<]
 EOF
 # ------------------------------------------------------------------------------------------------------- #
@@ -176,4 +187,9 @@ echo -en "\e[0m"
 
 # CLEANUP APT CACHE # ----------------------------------------------------------------------------------- #
 apt-get -y clean
+# ------------------------------------------------------------------------------------------------------- #
+
+
+# DISABLE GDM3 OR LIGHTDM # ----------------------------------------------------------------------------- #
+systemctl set-default multi-user.target
 # ------------------------------------------------------------------------------------------------------- #
